ee44a73af9691d2abf84eb84a428d71cded86eda
Peter Hagelund <peterhagelund@mac.com>
2024-03-14 18:19:07 -0400

    solutions to day 5 problems in go

---
+++ day5/cmd/puzzle1/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type move struct {
+	count  int
+	source int
+	target int
+}
+
+func main() {
+	file, err := os.Open("puzzle_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	temp := make([]string, 0)
+	var stacks [][]rune
+	var moves []*move
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if len(text) == 0 {
+			slices.Reverse(temp)
+			stacks = parseStacks(temp)
+			temp = make([]string, 0)
+			continue
+		}
+		temp = append(temp, text)
+	}
+	moves = parseMoves(temp)
+	message := makeMoves(stacks, moves)
+	fmt.Printf("message = %s\n", string(message))
+}
+
+func parseStacks(input []string) [][]rune {
+	fields := strings.Fields(input[0])
+	stacks := make([][]rune, len(fields))
+	for _, s := range input[1:] {
+		layer := []rune(s)
+		for i := 0; i < len(stacks); i++ {
+			if stacks[i] == nil {
+				stacks[i] = make([]rune, 0, 32)
+			}
+			r := layer[1+4*i]
+			if r != ' ' {
+				stacks[i] = append(stacks[i], r)
+			}
+		}
+	}
+	return stacks
+}
+
+func parseMoves(input []string) []*move {
+	moves := make([]*move, 0)
+	values := [3]int{}
+	for _, s := range input {
+		fields := strings.Fields(s)
+		for i := 0; i < 3; i++ {
+			value, err := strconv.Atoi(fields[2*i+1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			values[i] = value
+		}
+		moves = append(moves, &move{values[0], values[1], values[2]})
+	}
+	return moves
+}
+
+func makeMoves(stacks [][]rune, moves []*move) string {
+	for _, move := range moves {
+		for i := 0; i < move.count; i++ {
+			source := move.source - 1
+			target := move.target - 1
+			r := stacks[source][len(stacks[source])-1]
+			stacks[source] = stacks[source][:len(stacks[source])-1]
+			stacks[target] = append(stacks[target], r)
+		}
+	}
+	message := make([]rune, len(stacks))
+	for i := 0; i < len(stacks); i++ {
+		message[i] = stacks[i][len(stacks[i])-1]
+	}
+	return string(message)
+}
---
+++ day5/go.mod
@@ -0,0 +1,3 @@
+module github.com/peterhagelund/adventofcode2022/day5
+
+go 1.22.1
