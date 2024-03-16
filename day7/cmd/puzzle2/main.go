bf79993e78bd8dff5f5ccde90d11906eb0433d70
Peter Hagelund <peterhagelund@mac.com>
2024-03-15 20:49:57 -0400

    solutions to day 7 problems in go

---
+++ day7/cmd/puzzle2/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	file = iota
+	directory
+)
+
+type entry struct {
+	parent   *entry
+	kind     int
+	name     string
+	size     int
+	children []*entry
+}
+
+func (e *entry) totalSize() int {
+	if e.kind == file {
+		return e.size
+	} else {
+		totalSize := 0
+		for _, c := range e.children {
+			totalSize += c.totalSize()
+		}
+		return totalSize
+	}
+}
+
+func main() {
+	file, err := os.Open("puzzle_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	root := &entry{
+		parent:   nil,
+		kind:     directory,
+		name:     "/",
+		size:     0,
+		children: make([]*entry, 0),
+	}
+	dir := root
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.HasPrefix(text, "$ ") {
+			dir = executeCommand(text[2:], dir)
+		} else {
+			parseOutput(text, dir)
+		}
+	}
+	neededSize := 30000000 - (70000000 - root.totalSize())
+	smallestSize := 70000000
+	findSmallestPossibleSize(root, neededSize, &smallestSize)
+	fmt.Printf("smallest size = %d\n", smallestSize)
+}
+
+func executeCommand(cmd string, dir *entry) *entry {
+	if cmd == "ls" {
+		return dir
+	}
+	if strings.HasPrefix(cmd, "cd ") {
+		name := cmd[3:]
+		switch name {
+		case "/":
+			for dir.parent != nil {
+				dir = dir.parent
+			}
+			return dir
+		case "..":
+			return dir.parent
+		default:
+			for _, d := range dir.children {
+				if d.name == name {
+					return d
+				}
+			}
+			log.Fatal(fmt.Errorf("directory '%s' unknown", name))
+		}
+	}
+	return dir
+}
+
+func parseOutput(output string, dir *entry) {
+	if strings.HasPrefix(output, "dir ") {
+		name := output[4:]
+		newDir := &entry{
+			parent: dir,
+			kind:   directory,
+			name:   name,
+			size:   0,
+		}
+		dir.children = append(dir.children, newDir)
+	} else {
+		fields := strings.Fields(output)
+		size, err := strconv.Atoi(fields[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		name := fields[1]
+		newFile := &entry{
+			parent:   dir,
+			kind:     file,
+			name:     name,
+			size:     size,
+			children: nil,
+		}
+		dir.children = append(dir.children, newFile)
+	}
+}
+
+func findSmallestPossibleSize(e *entry, neededSize int, smallestSize *int) {
+	totalSize := e.totalSize()
+	if totalSize >= neededSize {
+		if totalSize < *smallestSize {
+			*smallestSize = totalSize
+		}
+	}
+	for _, c := range e.children {
+		if c.kind == directory {
+			findSmallestPossibleSize(c, neededSize, smallestSize)
+		}
+	}
+}
---
+++ day7/go.mod
@@ -0,0 +1,3 @@
+module github.com/peterhagelund/adventofcode2022/day7
+
+go 1.22.1
